930541ce3b8999574b9c4167fc2128d72cd4fd76
JiwooL0920 <JiwooL0920@192.168.2.17>
2019-05-09 14:53:29 -0400

    finished tutorial 9; added a back button; refactored game start/introMusic code

---
+++ Dynamic Beat/src/dynamic_beat_9/Main.java
@@ -0,0 +1,17 @@
+package dynamic_beat_9;
+
+public class Main {
+	//Public static--sharable variable throughout the project
+	//final--value does not change once initialized
+	//In general, for variable with "final", name them with all caps
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+
+	public static void main(String[] args) {
+
+		//Make a new instance using DynamicBeat class
+		new DynamicBeat();
+	}
+
+}
+
