1dee3f90a23e4b9c6a92b19d52b8385da060cb78
JiwooL0920 <JiwooL0920@192.168.2.17>
2019-05-09 15:57:27 -0400

    finished tutorial 10; desined game screen

--- Dynamic Beat/src/dynamic_beat_10/DynamicBeat.java
+++ Dynamic Beat/src/dynamic_beat_10/DynamicBeat.java
@@ -2,8 +2,11 @@ package dynamic_beat_10;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
@@ -36,12 +39,16 @@ public class DynamicBeat extends JFrame {
 
 	// retrieve background.jpg and put into background variable
 	// Having Main.class as a reference point get the resource from file path; reset
-	// variable background to that file						
+	// variable background to that file		
+	private Image gameInfoImage = new ImageIcon(Main.class.getResource("../images/gameInfo.png")).getImage(); 	
+	private Image judgementLineImage = new ImageIcon(Main.class.getResource("../images/judgementLine.png")).getImage(); 																									
+	private Image noteRouteImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage(); 																									
+	private Image noteRouteLineImage = new ImageIcon(Main.class.getResource("../images/noteRouteLine.png")).getImage(); 																									
+	private Image noteBasicImage = new ImageIcon(Main.class.getResource("../images/noteBasic.png")).getImage(); 																									
+
 	private Image background = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage(); 																									
 	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png"))); 
 	
-	//Images
-	private Image gameInfoImage = new ImageIcon(Main.class.getResource("../images/gameInfo.png")).getImage(); 																									
 
 	
 	//Buttons	
@@ -429,25 +436,85 @@ public class DynamicBeat extends JFrame {
 	public void paint(Graphics g) {
 		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT); // create image by the screen size
 		screenGraphic = screenImage.getGraphics();
-		screenDraw(screenGraphic); // draw the image we want on the screen
+		screenDraw((Graphics2D) screenGraphic); // Graphics2D is needed to printString nicely on the screen (otherwise it cracks)
 		g.drawImage(screenImage, 0, 0, null); // draw screenImage at point (0,0)
 	}
 
 	// 2) screenDraw method
 	// There are two ways to draw: drawimage and paintcomponents
-	public void screenDraw(Graphics g) {
+	public void screenDraw(Graphics2D g) {
 		g.drawImage(background, 0, 0, null); //generally, we use drawImage to draw moving images
 		if (isMainScreen) {
 			g.drawImage(selectedImage, 340, 100, null);
 			g.drawImage(titleImage, 340, 70, null);
 		}
-		if (isGameScreen) {
+		if (isGameScreen) {		
+			//noteRouteImage
+			g.drawImage(noteRouteImage, 228, 30, null);
+			g.drawImage(noteRouteImage, 332, 30, null);
+			g.drawImage(noteRouteImage, 436, 30, null);
+			g.drawImage(noteRouteImage, 540, 30, null);
+			g.drawImage(noteRouteImage, 640, 30, null);
+			g.drawImage(noteRouteImage, 744, 30, null);
+			g.drawImage(noteRouteImage, 848, 30, null);
+			g.drawImage(noteRouteImage, 952, 30, null);
+			
+			//noteRouteLineImage
+			g.drawImage(noteRouteLineImage, 224, 30, null);
+			g.drawImage(noteRouteLineImage, 328, 30, null);
+			g.drawImage(noteRouteLineImage, 432, 30, null);
+			g.drawImage(noteRouteLineImage, 536, 30, null);
+			g.drawImage(noteRouteLineImage, 740, 30, null);
+			g.drawImage(noteRouteLineImage, 844, 30, null);
+			g.drawImage(noteRouteLineImage, 948, 30, null);
+			g.drawImage(noteRouteLineImage, 1052, 30, null);
+			
+			//Others
 			g.drawImage(gameInfoImage, 0, 660, null);
+			g.drawImage(judgementLineImage, 0, 580, null);
+			
+			//noteBasicImage
+			g.drawImage(noteBasicImage, 228, 120, null);
+			g.drawImage(noteBasicImage, 332, 580, null);
+			g.drawImage(noteBasicImage, 436, 500, null);
+			g.drawImage(noteBasicImage, 540, 340, null);
+			g.drawImage(noteBasicImage, 640, 340, null);
+			g.drawImage(noteBasicImage, 744, 325, null);
+			g.drawImage(noteBasicImage, 848, 305, null);
+			g.drawImage(noteBasicImage, 952, 305, null);
+			
+			//Track Info
+			g.setColor(Color.white);
+			g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON); //prevent printString from cracking
+			g.setFont(new Font("Arial", Font.BOLD, 30));
+			g.drawString("Cool-Tobu", 20, 702);
+			
+			//Level
+			g.drawString("Easy", 1190, 700);;	
+	
+			//Key Info
+			g.setFont(new Font("Arial", Font.PLAIN, 26));
+			g.setColor(Color.DARK_GRAY);
+			g.drawString("S",270,609);
+			g.drawString("D", 374, 609);
+			g.drawString("F", 478, 609);
+			g.drawString("Space Bar", 580, 609);
+			g.drawString("J", 784, 609);
+			g.drawString("K",889,609);
+			g.drawString("L", 993, 609);
+			
+			//Score
+			g.setColor(Color.LIGHT_GRAY);
+			g.setFont(new Font("Elephant",Font.BOLD,30));
+			g.drawString("000000", 565, 702);
+			
 		}
 		paintComponents(g); // draws the images in the screen image (menubar stays constant; doesn't change. therefore use paintcomponent not drawimage), draws all the "add()" components
 		this.repaint(); // calling the paint method again
 		// Way this works: the computer paints the screen at every moment until the
 		// program terminates
+		
+		//Note: images that are declared later is drawn on top of ones that were declared before them
 	}
 	
 	//function for Track
@@ -485,13 +552,13 @@ public class DynamicBeat extends JFrame {
 		if (selectedMusic != null) 
 			selectedMusic.close();
 		isMainScreen = false;
+		isGameScreen = true;
 		leftButton.setVisible(false);
 		rightButton.setVisible(false);
 		easyButton.setVisible(false);
 		hardButton.setVisible(false);
 		background = new ImageIcon(Main.class.getResource("../images/"+trackList.get(nowSelected).getGameImage())).getImage();
 		backButton.setVisible(true);
-		isGameScreen = true;
 	}
 	
 	//Function to go back to the main screen
