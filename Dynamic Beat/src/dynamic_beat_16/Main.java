f98ce4cc87c6c14716b55d801c7c5c4ac1022583
JiwooL0920 <JiwooL0920@192.168.2.17>
2019-05-09 20:39:01 -0400

    finished tutorial 16; designed/implemented note judgment images; fully functional

---
+++ Dynamic Beat/src/dynamic_beat_16/Main.java
@@ -0,0 +1,20 @@
+package dynamic_beat_16;
+
+public class Main {
+	//Public static--sharable variable throughout the project
+	//final--value does not change once initialized
+	//In general, for variable with "final", name them with all caps
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+	public static final int NOTE_SPEED = 3; //the speed at which the note drops
+	public static final int SLEEP_TIME = 10; //in order to drop notes in evenly spanned time
+	public static final int REACH_TIME = 2; //time needed for note to reach the judge bar
+	
+	public static void main(String[] args) {
+
+		//Make a new instance using DynamicBeat class
+		new DynamicBeat();
+	}
+
+}
+
